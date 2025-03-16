d975247f38250d242aa0e7ebdb37f5e14ad2f7d2
Kanchai Lerdsrisakulrat <kanchai.lerd@kmutt.ac.th>
2025-03-16 22:43:11 +0700

    HTML/CSS all done

--- telepathy_frontend/src/components/Navbar.jsx
+++ telepathy_frontend/src/components/Navbar.jsx
@@ -2,17 +2,41 @@ import './Navbar.css'
 import profileUser from '../assets/profile1.jpg'
 
 const Navbar = () => {
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    }
+
     return(
-        <div className="navbar">
+        <div>
+          <div className="navbar">
             <div className="profile">
-                <img src={profileUser}></img>
+                <img src={profileUser} />
                 <p>UserID</p>
             </div>
             <div className="back-btn">
-                <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M12 28H6.66667C5.95942 28 5.28115 27.719 4.78105 27.219C4.28095 26.7189 4 26.0406 4 25.3333V6.66667C4 5.95942 4.28095 5.28115 4.78105 4.78105C5.28115 4.28095 5.95942 4 6.66667 4H12M21.3333 22.6667L28 16M28 16L21.3333 9.33333M28 16H12" stroke="#303030" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+                <svg width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M10 26H4.66667C3.95942 26 3.28115 25.719 2.78105 25.219C2.28095 24.7189 2 24.0406 2 23.3333V4.66667C2 3.95942 2.28095 3.28115 2.78105 2.78105C3.28115 2.28095 3.95942 2 4.66667 2H10M19.3333 20.6667L26 14M26 14L19.3333 7.33333M26 14H10" stroke="#303030" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
                 </svg>
             </div>
+          </div>
+          <div className="tb-container">
+            <form className='tb-form' onSubmit={handleSubmit}>
+                <input type="text" ></input>
+                <button className='tb-btn' type='submit'>
+                    <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <g clip-path="url(#clip0_53_42)">
+                        <path d="M18.3332 1.66666L9.1665 10.8333M18.3332 1.66666L12.4998 18.3333L9.1665 10.8333M18.3332 1.66666L1.6665 7.49999L9.1665 10.8333" stroke="#F5F5F5" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                        </g>
+                        <defs>
+                        <clipPath id="clip0_53_42">
+                        <rect width="20" height="20" fill="white"/>
+                        </clipPath>
+                        </defs>
+                    </svg>
+                </button>
+            </form>
+          </div>
         </div>
     )
 }
--- telepathy_frontend/src/pages/ChatingPage.jsx
+++ telepathy_frontend/src/pages/ChatingPage.jsx
@@ -1,12 +1,36 @@
 import './ChatingPage.css'
 import Navbar from '../components/Navbar'
-import TypingBox from '../components/TypingBox'
+import userProfile from '../assets/profile1.jpg'
 
 const ChatingPage = () => {
+
+    const myID = 'KL3475'
+    const message = [
+        {
+            text: 'that looks so good!',
+            userid: 'KL3475'
+        },
+        {
+            text: 'or we could make this?',
+            userid: 'TN3449'
+        }
+    ]
     return(
-        <div className="cp-container">
+        <div>
             <Navbar />
-            <TypingBox />
+            <div className="chatbox-container">
+                <div className="other-message">
+                    <img src={userProfile} />
+                    <div className="other-text">
+                        <p>that looks so good!</p>
+                    </div>
+                </div>
+                <div className="my-message">
+                    <div className="my-text">
+                        <p>or we could make this?</p>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
