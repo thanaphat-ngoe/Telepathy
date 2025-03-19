f4174a77c1b4ca26a13bb9d20a4348fa420d2ea3
Kanchai Lerdsrisakulrat <kanchailerdsrisakulrat@gmail.com>
2025-03-19 23:37:50 +0700

    ProfilePage Done

--- telepathy_frontend/src/store/useAuthStore.js
+++ telepathy_frontend/src/store/useAuthStore.js
@@ -59,5 +59,22 @@ export const useAuthStore = create((set) => ({
         } finally {
             set({ isLoggingOut: false });
         }
+    },
+
+    updateProfile: async (data) => {
+        set({ isUpdatingProfile: true });
+        try {
+            const res = await axiosInstance.put("/auth/update-profile", data);
+            set({ authUser: res.data });
+            toast.success("Profile updated successfully");
+        }
+        catch (error) {
+            console.log("error in update profile:", error);
+            toast.error(error.response.data.message); 
+        }
+        finally {
+            set({ isUpdatingProfile: false })
+        }
     }
+
 }));
