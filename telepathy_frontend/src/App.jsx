5b61f6a455b64f591449354669337c8fea52851a
KUNG <thanaphat.ngoe@gmail.com>
2025-03-21 10:33:05 +0700

    Added Components

--- telepathy_frontend/src/App.jsx
+++ telepathy_frontend/src/App.jsx
@@ -1,50 +1,47 @@
-import Navbar from './components/Navbar';
-import HomePage from './pages/HomePage';
-import SignUpPage from './pages/SignUpPage';
-import LoginPage from './pages/LoginPage';
-import SettingsPage from './pages/SettingsPage';
-import ProfilePage from './pages/ProfilePage';
-
-import { useAuthStore } from './store/useAuthStore';
-
+import { useEffect } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
-import { useEffect } from 'react';
-import { Loader } from 'lucide-react';
+import { Loader } from "lucide-react";
+import { Toaster } from "react-hot-toast";
 
-import { Toaster } from 'react-hot-toast';
-import { useThemeStore } from './store/useThemeStore';
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import SignUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/LoginPage";
+import SettingsPage from "./pages/SettingsPage";
+import ProfilePage from "./pages/ProfilePage";
 
+import { useAuthStore } from "./store/useAuthStore";
+import { useThemeStore } from "./store/useThemeStore";
 
 const App = () => {
     const { authUser, checkAuth, isCheckingAuth, isLoggingOut } = useAuthStore();
-    const {theme} = useThemeStore()
+    const { theme } = useThemeStore();
 
     useEffect(() => {
         checkAuth();
     }, [checkAuth]);
 
-    console.log({ authUser });
+    console.log(authUser);
 
     if ((isCheckingAuth && !authUser) || isLoggingOut) return (
-        <div className='flex items-center justify-center h-screen'>
-            <Loader className='size-10 animate-spin'/>
+        <div className="flex items-center justify-center h-screen">
+            <Loader className="size-10 animate-spin" />
         </div>
-    )
+    );
 
     return (
         <div data-theme={theme}>
             <Navbar />
-                <Routes>
-                    <Route path='/' element={authUser ? <HomePage /> : <Navigate to="/loginAuth" />} />
-                    <Route path='/registerAuth' element={!authUser ? <SignUpPage /> : <Navigate to="/" />} />
-                    <Route path='/loginAuth' element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
-                    <Route path='/settings' element={<SettingsPage />} />
-                    <Route path='/profile' element={authUser ? <ProfilePage /> : <Navigate to="/loginAuth" />} />
-                </Routes>
-
-                <Toaster/>
+            <Routes>
+                <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/loginAuth" />} />
+                <Route path="/registerAuth" element={!authUser ? <SignUpPage /> : <Navigate to="/" />} />
+                <Route path="/loginAuth" element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
+                <Route path="/settings" element={<SettingsPage />} />
+                <Route path="/profile" element={authUser ? <ProfilePage /> : <Navigate to="/loginAuth" />} />
+            </Routes>
+        <Toaster />
         </div>
-    )
+    );
 };
 
 export default App;
--- telepathy_frontend/tailwind.config.js
+++ telepathy_frontend/tailwind.config.js
@@ -41,6 +41,6 @@ export default {
             "dim",
             "nord",
             "sunset",
-          ],
+        ],
     },
 };
